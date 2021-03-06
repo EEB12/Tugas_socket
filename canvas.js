e18b9982a31425f32bb1eca7951f3289e3cf2682
EEB12 <rayhan.taqara@yahoo.com>
2021-03-06 17:52:46 +0700

    tes

---
+++ canvas.js
@@ -0,0 +1,56 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#canvas");
+    const context = canvas.getContext("2d");
+
+    
+    
+    
+
+    // Resizing canvas box
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    // context.fillRect(100, 20, 200, 200);
+    //variables 
+    let painting = false;
+
+    function startPosition(){
+        painting = true;
+    }
+    function finishedPosition(){
+        painting = false;
+        context.beginPath();
+    }
+
+    function draw(e){
+        if(!painting) return;
+        context.lineWidth = 5;
+        context.lineCap = "round";
+
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
+    }
+    //event listener
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("click",draw);
+
+    
+
+});
+
+
+window.addEventListener("resize", () => {
+   
+    var wrapper = document.getElementById("signature-pad");
+    var canvas = wrapper.querySelector("canvas");
+    var ratio = Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+})
+
