bc7c90cc177821afae3a503a82a5610861de420c
ArchieOps <jonathanleonardo123@gmail.com>
2021-03-06 16:58:52 +0700

    Pembuatan Canvas HTML 5

---
+++ canvas.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#canvas");
+    const context = canvas.getContext("2d");
+
+    //Resizing canvas box
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    // context.fillRect(100, 20, 200, 200);
+    //variables 
+    let painting = false;
+
+    function startPosition(){
+        painting = true;
+    }
+    function finishedPosition(){
+        painting = false;
+        context.beginPath();
+    }
+
+    function draw(e){
+        if(!painting) return;
+        context.lineWidth = 5;
+        context.lineCap = "round";
+
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
+    }
+    //event listener
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+});
