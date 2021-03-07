98d8f2ec538c6514470bebca337e3d1776a3995d
EEB12 <rayhan.taqara@yahoo.com>
2021-03-07 16:55:50 +0700

    tombol erase dan pen work

--- canvas.js
+++ canvas.js
@@ -1,7 +1,12 @@
-window.addEventListener("load", () => {
-    const canvas = document.querySelector("#canvas");
-    const context = canvas.getContext("2d");
 
+
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
+var mode="pen"; 
+
+window.addEventListener("load", () => {
+    
+     
     //Resizing canvas box
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
@@ -22,33 +27,46 @@ window.addEventListener("load", () => {
         if(!painting) return;
         context.lineWidth = 3;
         context.lineCap = "round";
-
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
+         if(mode == "pen"){
+            context.globalCompositeOperation="source-over"
+            context.strokeStyle="red"
+            context.lineTo(e.clientX, e.clientY);
+            context.stroke();
+            context.beginPath();
+            context.moveTo(e.clientX, e.clientY);
+        }
+        
+        else if(mode=="eraser"){
+            
+            context.globalCompositeOperation="destination-out";
+            context.arc(e.clientX,e.clientY,20,0,Math.PI*2,false);
+            context.fill();
+        }
+        
+        
     }
     //event listener
     
-    function pen(){
-        mode = "pen"
-    }
-    function eraser(){
-        mode = "eraser"
-    }
+    
 
     
    
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
     canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("click",draw);
     
-    $("#pen").addEventListener("click",pen);  
-    $("#eraser").addEventListener("click",eraser);
-    var mode="eraser";  
-    // $("#pen").click(function(){ mode="pen"; });  
-    // $("#eraser").click(function(){ mode="eraser"; });
+    var btn1 = document.getElementById("eraser");
+
+    var btn2 = document.getElementById("pen");
+
+
+    btn1.addEventListener("click", function(e) {
+    mode="eraser"
+    });
+
+    btn2.addEventListener("click", function(e) {
+        mode="pen"
+    });
 
     
     
@@ -66,3 +84,4 @@ window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
 
 });
+
