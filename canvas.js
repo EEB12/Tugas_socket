4275b602cfbf8e0d28a29aca9dbf2cb7b448c5fe
EEB12 <rayhan.taqara@yahoo.com>
2021-03-07 15:47:34 +0700

    Revert "Merge branch 'taqarra' into Testing"
    This reverts commit d9d4a956546933d3080a7a63434201c436100375, reversing
    changes made to 85a25a0c1d445a024600788aaf01e59c676c81f0.

--- canvas.js
+++ canvas.js
@@ -2,11 +2,7 @@ window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const context = canvas.getContext("2d");
 
-    
-    
-    
-
-    // Resizing canvas box
+    //Resizing canvas box
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
@@ -24,64 +20,28 @@ window.addEventListener("load", () => {
 
     function draw(e){
         if(!painting) return;
-        
-        // if (mode=="pen"){
-            context.lineWidth = 5;
-            context.lineCap = "round";
-            if(mode=="eraser"){
-                // context.globalCompositeOperation = "destination-out";
-                // context.strokeStyle = "rgba(0,0,0,1)";
-                context.strokeStyle="red"
-            }
-            
-            context.lineTo(e.clientX, e.clientY);
-            context.stroke();
-            context.beginPath();
-            context.moveTo(e.clientX, e.clientY);
-        // } else{
-        //     context.globalCompositeOperation="destination-out";
-        //     context.arc(e.clientX, e.clientY,8,0,Math.PI*2,false);
-        //     context.fill();
-        // }
-        
+        context.lineWidth = 3;
+        context.lineCap = "round";
+
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
     }
     //event listener
-    
-    function pen(){
-        mode = "pen"
-    }
-    function eraser(){
-        mode = "eraser"
-    }
-
-    
-   
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
     canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("click",draw);
-    
-    $("#pen").addEventListener("click",pen);  
-    $("#eraser").addEventListener("click",eraser);
-    var mode="eraser";  
-    // $("#pen").click(function(){ mode="pen"; });  
-    // $("#eraser").click(function(){ mode="eraser"; });
-
-    
-    
-
 });
 
-
 window.addEventListener("resize", () => {
-   
     var wrapper = document.getElementById("signature-pad");
     var canvas = wrapper.querySelector("canvas");
     var ratio = Math.max(window.devicePixelRatio || 1, 1);
     canvas.width = canvas.offsetWidth * ratio;
     canvas.height = canvas.offsetHeight * ratio;
+
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-})
 
-
+});
